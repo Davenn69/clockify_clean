822398825f92e28958e064dff934b7c8ec1dc249
Davenn69 <devendeven98@gmail.com>
2025-03-24 10:36:59 +0700

    Add skeletonizer

--- lib/features/activity/presentation/widgets/history_widgets.dart
+++ lib/features/activity/presentation/widgets/history_widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../core/utils/date_formatting.dart';
 import '../../application/providers/history_providers.dart';
@@ -12,13 +13,40 @@ List<Widget> makeWidget(BuildContext context, WidgetRef ref, List<ActivityHive> 
   final historyState = ref.watch(historyHiveStateNotifierProvider);
   final type = ref.watch(selectedChoiceProvider)?? "Latest Date";
 
-  if (historyState.history == null || historyState.history.isEmpty) {
+  bool isLoading = historyState.history == null || historyState.history.isEmpty;
+
+  ActivityHive temp = ActivityHive(uuid: "", description : "", startTime: DateTime.now(), endTime: DateTime.now(), locationLat: 0, locationLng: 0, createdAt: DateTime.now(), updatedAt: DateTime.now(), userUuid: "");
+
+  if (isLoading) {
     return [
       FutureBuilder(
         future: Future.delayed(Duration(milliseconds: 1500)),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return Skeletonizer(
+              enabled: true,
+                effect: ShimmerEffect(
+                    baseColor: Colors.grey[600]!,
+                    highlightColor: Colors.grey[500]!,
+                  duration: Duration(milliseconds: 1000),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref),
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref),
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref),
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref),
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref),
+                    activityDateWidget(DateTime.now()),
+                    activityHistoryWidget(temp, context, ref)
+                  ],
+                )
+            );
           }
           return Center(
             child: Text(
